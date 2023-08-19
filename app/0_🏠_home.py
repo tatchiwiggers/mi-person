fe2277fd6f28588dffbdd0469162a557fb4968c4
tabatha wiggers <tatisabato@hotmail.com>
2023-08-19 12:51:09 -0300

    small fixes

--- app/0_🏠_home.py
+++ app/0_🏠_home.py
@@ -1,17 +1,17 @@
 import streamlit as st
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+import matplotlib.pyplot as plt
 
 st.set_page_config(
-    page_title="mi-person", # => Quick reference - Streamlit
+    page_title="mi-person",  # => Quick reference - Streamlit
     page_icon="🏠",
-    layout="centered", # wide
-    initial_sidebar_state="auto") # auto - prefixed bar
+    layout="centered",  # wide
+    initial_sidebar_state="auto",
+)  # auto - prefixed bar
 
 
-
-
-with open('style.css') as f:
-    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-
+with open("./style.css") as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
     def intro():
         import streamlit as st
@@ -20,7 +20,7 @@ with open('style.css') as f:
         st.sidebar.success("What would you like to do?")
 
         st.markdown(
-        """
+            """
             mi-person is an open-source web application built specifically to help
             people understand each other!
             **👈 Select an action from the dropdown on the left** to see some examples
@@ -29,10 +29,8 @@ with open('style.css') as f:
         )
 
     def mapping_emotions():
-        from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
-        import matplotlib.pyplot as plt
-
-
+        
+        
 
         st.markdown(f"# {list(page_names_to_funcs.keys())[2]}")
         st.write(
@@ -46,14 +44,19 @@ with open('style.css') as f:
         # text = 'Fun, fun, awesome, awesome, tubular, astounding, superb, great, amazing, amazing, amazing, amazing'
         text = "I, hate, hate, fun, fun, awesome, awesome, awesome, believe, can't, amazing, amazing, amazing"
 
-
         # Create and generate a word cloud image:
-        wordcloud = WordCloud(stopwords=stopwords, background_color= "white", width=440, height=280, colormap='Set3').generate(text)
+        wordcloud = WordCloud(
+            stopwords=stopwords,
+            background_color="white",
+            width=440,
+            height=280,
+            colormap="Set3",
+        ).generate(text)
 
         # Display the generated image:
 
-        fig, ax = plt.subplots(figsize = (12, 8))
-        ax.imshow(wordcloud, interpolation='bilinear')
+        fig, ax = plt.subplots(figsize=(12, 8))
+        ax.imshow(wordcloud, interpolation="bilinear")
         plt.axis("off")
         st.pyplot(fig)
 
@@ -61,7 +64,7 @@ with open('style.css') as f:
         import streamlit as st
         import seaborn as sns
 
-        st.markdown(f'# {list(page_names_to_funcs.keys())[1]}')
+        st.markdown(f"# {list(page_names_to_funcs.keys())[1]}")
         st.write(
             """
             This plot illustrates the combination of sentiments found in the text.
@@ -74,7 +77,6 @@ with open('style.css') as f:
             """
         )
 
-    
         import streamlit as st
         import pandas as pd
         import numpy as np
@@ -82,23 +84,28 @@ with open('style.css') as f:
         from plotly.subplots import make_subplots
         import plotly.graph_objects as go
         import matplotlib.pyplot as plt
+
         train = pd.read_csv("emotion.csv")
 
-
-        sentiments = train['description']
+        sentiments = train["description"]
         sentiments = pd.DataFrame(sentiments)
         # s = sentiments.rename(columns={'description': 'description', 'description': 'rate'})
 
         chart_data = pd.DataFrame(
             [[7.0], [18.26], [9.5], [11.42], [18.02], [35.80]],
-            columns=['rate'],
-            index=['dispapproval', 'excitement', 'joy',
-                   'optimism', 'surprise', 'neutral']
+            columns=["rate"],
+            index=[
+                "dispapproval",
+                "excitement",
+                "joy",
+                "optimism",
+                "surprise",
+                "neutral",
+            ],
         )
 
         st.bar_chart(chart_data)
-    
-        
+
     page_names_to_funcs = {
         "—": intro,
         "Plotting the sentiments of your text": plotting_sentiment,
--- app/pages/1_👤_mi-person.py
+++ app/pages/1_👤_mi-person.py
@@ -1,11 +1,11 @@
-from select import select
+# from select import select
 import streamlit as st
 import pandas as pd
-import csv
-from PIL import Image
-import numpy as np
-import json
-import requests
+# import csv
+# from PIL import Image
+# import numpy as np
+# import json
+# import requests
 # from pre import sentiment_scores
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
@@ -13,19 +13,16 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 # PAGE_CONFIGURATION
 
 st.set_page_config(
-    page_title="mi-person", # => Quick reference - Streamlit
+    page_title="mi-person",  # => Quick reference - Streamlit
     page_icon="🏠",
-    layout="centered", # wide
-    initial_sidebar_state="auto") # auto - prefixed bar
+    layout="centered",  # wide
+    initial_sidebar_state="auto")  # auto - prefixed bar
 
 
 # HOME_PAGE
 
-with open('style.css') as f:
+with open('./style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-
-
-
     st.markdown("""# mi-person""")
     st.markdown("""### **say the right thing at the right time**
     """)
@@ -42,7 +39,7 @@ with open('style.css') as f:
 
     sentence = st.text_input(label='Enter a sentence to be analyzed:')
 
-    user_input = {'sentence': sentence }
+    user_input = {'sentence': sentence}
 
     def sentiment_scores(sentence):
         # Create a SentimentIntensityAnalyzer object.
@@ -52,36 +49,34 @@ with open('style.css') as f:
         # object gives a sentiment dictionary.
         # which contains pos, neg, neu, and compound scores.
         sentiment_dict = sid_obj.polarity_scores(sentence)
-        
+
         print("Overall sentiment: ", sentiment_dict)
         print("sentence was rated as ", sentiment_dict['neg']*100, "% Negative")
         print("sentence was rated as ", sentiment_dict['neu']*100, "% Neutral")
         print("sentence was rated as ", sentiment_dict['pos']*100, "% Positive")
 
-        print("\nSentence overall rated as", end = " ",)
+        print("\nSentence overall rated as", end=" ",)
 
         # decide sentiment as positive, negative and neutral
         # sentence = input("Enter a sentence to be analyzed: ")
 
         # sentiment_scores(sentence)
         return sentiment_dict
-    
 
     if st.button("Do the magic"):
         # res = requests.post(url ='http://127.0.0.1:8504/mi-person', data=json.dumps(user_input))
-        
+
         sentiment_dict = sentiment_scores(sentence)
-        
-        if sentiment_dict['compound'] >= 0.05 :
+
+        if sentiment_dict['compound'] >= 0.05:
             res = "Positive, say something nice back :)"
 
-        elif sentiment_dict['compound'] <= - 0.05 :
+        elif sentiment_dict['compound'] <= - 0.05:
             res = "Negative, try to chill a bit before answering..."
 
-        else :
+        else:
             res = "Neutral, so no problem here ;)"
-            
-        
+
         if res.split(',')[0] == 'Positive':
             # st.subheader('Sucesso')
             st.success(f'sentiment: {res}')
@@ -96,7 +91,7 @@ with open('style.css') as f:
             st.error('The model could not evaluate any emotion in the text')
 
         st.subheader('Individual percentages per emotion')
-        
+
         s = pd.DataFrame([sentiment_dict])
         s = s[['neg', 'pos', 'neu']]
         s = s.rename(columns={'neg': 'negative', 'pos': 'positive', 'neu': 'neutral'}).reset_index(drop=True)
@@ -115,8 +110,6 @@ with open('style.css') as f:
 
         # Display a static table
         st.table(s)
-    
-    
         st.markdown('To know more about our product:')
         # st.markdown('[**about mi-person**](http://192.168.0.8:8501/about-us)',False)
         st.write("check out our github repository @ [mi-person repository](https://github.com/tatchiwiggers/mi-person)")
